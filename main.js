0af52d37c356a49b9f9e307e24ce45a316c702d1
JGerardoToledoR <jgerardo.toledor@gmail.com>
2025-11-05 15:06:10 -0600

    [UPDATE] cambios

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator){
     window.addEventListener('load', () =>{
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('/pwa_practica/sw.js')
         .then(reg => {
             console.log('SW Registrado', reg)
         })
--- sw.js
+++ sw.js
@@ -2,15 +2,15 @@ const STATIC_CACHE = 'app-shell-v2';
 const DYNAMIC_CACHE = 'dynamic-cache-v1';
 
 const ASSETS_APP_SHELL = [
-  './',
-  './home.html',
-  './calendario.html',
-  './formulario.html',
-  './main.js',
+  './pwa_practica/',
+  './pwa_practica/home.html',
+  './pwa_practica/calendario.html',
+  './pwa_practica/formulario.html',
+  './pwa_practica/main.js',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
-  '/assets/icons/192.png',
-  '/assets/icons/512.png'
+  '/pwa_practica/assets/icons/192.png',
+  '/pwa_practica/assets/icons/512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -75,6 +75,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(request)
       .then(response => response || fetch(request))
-      .catch(() => caches.match('./home.html'))
+      .catch(() => caches.match('./pwa_practica/home.html'))
   );
 });
